52947fef781932e90debb4dfcfa98f74c0b47277
Adrian Olave <olaveves@gmail.com>
2021-12-21 17:44:43 -0300

    Agrego NavBar y Trato de agregar Carrito

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
-import { NavBar } from './components/NavBar';
+import './components/Header/Header.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from './components/Header/NavBar/NavBar';
+// import { CartWidget } from './components/NavBar/CartWidget';
+
 
 function App() {
 
   return (
+    <header id="header">
       <NavBar />
+    </header>
+      
   );
 }
 
